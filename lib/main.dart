43104867a384586ba6b8dcf3d5ada9b8cf3bcd75
waritph <warit_phanta@cmu.ac.thm>
2026-01-09 11:05:21 +0700

    newest

--- lib/main.dart
+++ lib/main.dart
@@ -31,6 +31,8 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   late final WebViewController _controller;
+  late final WebViewController _controller1;
+  late final WebViewController _controller2;
 
   void initState() {
     super.initState();
@@ -52,11 +54,13 @@ class _MyHomePageState extends State<MyHomePage> {
             return NavigationDecision.prevent;
           },
         ),
-      ); // ✅ ปิดวงเล็บ/ปิด chain ให้ครบตรงนี้
+      ); 
 
     //..loadRequest(Uri.parse("https://flutter.dev/"));
     _controller.loadRequest(Uri.parse("https://flutter.dev/"));
     _controller.loadFlutterAsset('assets/index.html');
+    _controller1 = WebViewController()..loadFlutterAsset('assets/index.html');
+    _controller2 = WebViewController()..loadHtmlString("<html><head><title>Local Asset</title></head><body><h1>Hello from assets/index.html</h1><p>This is loaded inside WebView.</p></body></html>");
   }
 
   @override
@@ -68,25 +72,32 @@ class _MyHomePageState extends State<MyHomePage> {
           IconButton(
             icon: Icon(Icons.arrow_back),
             onPressed: () async {
-              if (await _controller.canGoBack()) {
-                _controller.goBack();
+              if (await _controller1.canGoBack()) {
+                _controller1.goBack();
               }
             },
           ),
           IconButton(
             icon: Icon(Icons.arrow_forward),
             onPressed: () async {
-              if (await _controller.canGoForward()) {
-                _controller.goForward();
+              if (await _controller1.canGoForward()) {
+                _controller1.goForward();
               }
             },
           ),
           IconButton(
               icon: Icon(Icons.refresh),
-              onPressed: () => _controller.reload()),
+              onPressed: () => _controller1.reload()),
         ],
       ),
-      body: WebViewWidget(controller: _controller),
+      body: 
+       Column(
+        children: [
+          Expanded(child: WebViewWidget(controller: _controller1)),
+          const Divider(thickness: 2, height: 1),
+          Expanded(child: WebViewWidget(controller: _controller2)),
+        ]
+       ),
     );
   }
 }
